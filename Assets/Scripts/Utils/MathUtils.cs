cbc3b9bd92696c4ac06714d063a61e1f066ad6db
Bastian Blokland <bastianblokland@gmail.com>
2018-05-01 00:04:43 +0200

    Added a fast Vector3 normalize method based on the method used in quake3 (https://en.wikipedia.org/wiki/Fast_inverse_square_root)

--- Assets/Scripts/Utils/MathUtils.cs
+++ Assets/Scripts/Utils/MathUtils.cs
@@ -5,6 +5,34 @@ namespace Utils
 {
 	public static class MathUtils
 	{
+		[StructLayout(LayoutKind.Explicit, Size = 4)]
+		private struct IntFloat 
+		{
+			[FieldOffset(0)]
+			public float FloatValue;
+
+			[FieldOffset(0)]
+			public int IntValue;
+		}
+
+		//Implementation based on: https://en.wikipedia.org/wiki/Fast_inverse_square_root
+		public static float FastInvSqrRoot(float number)
+		{
+			IntFloat conv = new IntFloat { FloatValue = number }; 
+
+			float x2 = number * .5f;
+	 		conv.IntValue = 0x5f3759df - (conv.IntValue >> 1);
+	 		conv.FloatValue = conv.FloatValue * (1.5f - (x2 * conv.FloatValue * conv.FloatValue));
+	 		return conv.FloatValue;
+		}
+
+		public static Vector3 FastNormalize(Vector3 vector)
+		{
+			float sqrMag = vector.sqrMagnitude;
+			float invSqrRoot = FastInvSqrRoot(sqrMag);
+			return new Vector3(vector.x * invSqrRoot, vector.y * invSqrRoot, vector.z * invSqrRoot);
+		}
+
 		public static bool DoesRangeOverlap(float aStart, float aEnd, float bStart, float bEnd)
 		{
 			return aEnd >= bStart && aStart <= bEnd;
