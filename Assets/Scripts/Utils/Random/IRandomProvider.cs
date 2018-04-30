780d9f189edca092d5877a3b4442854bce0a2150
Bastian Blokland <bastianblokland@gmail.com>
2018-05-01 00:06:55 +0200

    Created simple thread-safe random utility based on: https://en.wikipedia.org/wiki/Linear-feedback_shift_register

---
+++ Assets/Scripts/Utils/Random/IRandomProvider.cs
@@ -0,0 +1,7 @@
+﻿namespace Utils
+{
+	public interface IRandomProvider
+	{
+		float GetNext();
+	}
+}
---
+++ Assets/Scripts/Utils/Random/RandomUtils.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Utils
+{
+	public static class RandomUtils
+	{
+		public static Vector3 Direction(this IRandomProvider random)
+		{
+			Vector3 dir = new Vector3
+			(
+				x: random.GetNext() - .5f, 
+				y: random.GetNext() - .5f,
+				z: random.GetNext() - .5f
+			);
+			if(dir == Vector3.zero) //Should be very rare
+				return Vector3.forward;
+			return MathUtils.FastNormalize(dir);
+		}
+
+		public static int Between(this IRandomProvider random, int minValue, int maxValue)
+		{
+			return Mathf.FloorToInt(random.Between((float)minValue, (float)maxValue));
+		}
+
+		public static float Between(this IRandomProvider random, float minValue, float maxValue)
+		{
+			return minValue + (maxValue - minValue) * random.GetNext();
+		}
+	}
+}
