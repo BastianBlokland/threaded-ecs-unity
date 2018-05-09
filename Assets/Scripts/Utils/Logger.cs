6b4172f9ee6e2cd2d38a1590f8b4ce7cab18cbd8
Bastian Blokland <bastianblokland@gmail.com>
2018-05-09 21:47:29 +0300

    Implemented simple logger that can be used to log messages between threads.

---
+++ Assets/Scripts/Utils/Logger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Utils
+{
+	public sealed class Logger
+	{
+		private readonly Action<string> printDelegate;
+		private readonly Queue<string> logEntries;
+		private readonly object lockObject;
+
+		public Logger(Action<string> printDelegate)
+		{
+			this.printDelegate = printDelegate;
+
+			logEntries = new Queue<string>();
+			lockObject = new object();
+		}
+
+		public void Log(Exception exception) => Log($"Exception: '{exception.Message}'");
+
+		public void Log(string message)
+		{
+			lock(lockObject)
+			{
+				logEntries.Enqueue(message);
+			}	
+		}
+
+		public void Print()
+		{
+			lock(lockObject)
+			{
+				while(logEntries.Count > 0)
+					printDelegate?.Invoke(logEntries.Dequeue());
+			}
+		}
+	}
+}
