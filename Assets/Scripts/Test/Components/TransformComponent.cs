3682357781aef12e385444c2c9562fa1f170013b
Bastian Blokland <bastian@armadainteractive.com>
2018-05-12 01:19:24 +0300

    Created override for creating a TransformComponent with a direction

--- Assets/Scripts/Test/Components/TransformComponent.cs
+++ Assets/Scripts/Test/Components/TransformComponent.cs
@@ -2,6 +2,9 @@ using ECS.Storage;
 using UnityEngine;
 using Utils;
 
+using static UnityEngine.Vector3;
+using static Utils.MathUtils;
+
 namespace Test.Components
 {
     public struct TransformComponent : IComponent
@@ -12,5 +15,14 @@ namespace Test.Components
 		{
 			Matrix = Float3x4.FromPosition(position);
 		}
+
+        public TransformComponent(Vector3 position, Vector3 forward) : this(position, forward, Vector3.up) { }
+
+        public TransformComponent(Vector3 position, Vector3 forward, Vector3 upReference)
+        {
+            Vector3 right = FastNormalize(Cross(upReference, forward));
+            Vector3 up = FastNormalize(Cross(forward, right));
+            Matrix = Float3x4.FromPositionAndAxis(position, right, up, forward);
+        }
     }
 }
