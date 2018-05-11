4eafc7c8e0aae606046038012c82f94306652f4b
Bastian Blokland <bastian@armadainteractive.com>
2018-05-12 01:18:57 +0300

    Added simple Vector2 to Vector3 swizzle (will make more when i need them)

---
+++ Assets/Scripts/Utils/Swizzle.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Utils
+{
+	public static class Swizzle
+	{
+		public static Vector3 X0Y(this Vector2 vector)
+		{
+			return new Vector3(vector.x, 0f, vector.y);
+		}
+	}
+}
